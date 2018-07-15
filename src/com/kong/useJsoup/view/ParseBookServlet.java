a27275c08f71d600628fa4ae28ca37b0c8cf75cb
konglinghu777 <2710404562@qq.com>
2018-07-15 14:25:21 +0800

    J2ee learn .

--- src/com/kong/useJsoup/view/ParseBookServlet.java
+++ src/com/kong/useJsoup/view/ParseBookServlet.java
@@ -2,6 +2,8 @@ package com.kong.useJsoup.view;
 
 import com.kong.useJsoup.model.parseBookDetial;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,17 +11,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "ParseBookServlet")
 public class ParseBookServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String bookId = request.getParameter("bookid");
-        System.out.println(bookId);
+        //System.out.println(bookId);
         parseBookDetial pbd = new parseBookDetial();
         pbd.setBookId(bookId);
 
-        Document document = pbd.bookTitle();
-        request.setAttribute("allDet",document);
+        List el = pbd.parseHead();
+        //System.out.println(pbd.parseHead());
+        request.setAttribute("allDet",el);
         request.getRequestDispatcher("bookDetial.jsp").forward(request,response);
     }
 
