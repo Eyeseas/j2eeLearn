1eaff20462607e2a38735d06ea4f3bd7e3436539
konglinghu777 <2710404562@qq.com>
2018-07-15 14:26:02 +0800

    J2ee learn .

--- src/com/kong/useJsoup/model/parseBookDetial.java
+++ src/com/kong/useJsoup/model/parseBookDetial.java
@@ -2,8 +2,13 @@ package com.kong.useJsoup.model;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import javax.print.Doc;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class parseBookDetial {
 
@@ -17,8 +22,54 @@ public class parseBookDetial {
         this.bookId = bookId;
     }
 
-    public Document bookTitle() throws IOException {
-        Document doc = Jsoup.connect("https://www.biquge5200.cc/"+bookId).get();
-        return doc;
+//    public List bookTitle() throws IOException {
+//        //整个网页文档
+//        Document doc = Jsoup.connect("https://www.biquge5200.cc/"+bookId).get();
+//        List titlePart = new ArrayList();
+//        Elements bookinfo = doc.getElementsByTag("h1");
+//        String booktitle = bookinfo.html();
+//
+//        titlePart.add(booktitle);
+//
+//        return titlePart;
+//    }
+
+
+//    获取整个页面文档
+    public Document getConnect() throws IOException {
+        return Jsoup.connect("https://www.biquge5200.cc/"+bookId).get();
     }
+
+//    处理头部简介
+
+    public List parseHead() throws IOException {
+        List titlePart = new ArrayList();
+
+        Document doc = getConnect();
+
+        //        小说名称
+        Element title1 = doc.getElementById("info");
+        Elements bookNameEl = title1.getElementsByTag("h1");
+        String bookName = bookNameEl.html();
+        titlePart.add(bookName);
+
+        Elements authorTimer = title1.getElementsByTag("p");
+        String authorName = authorTimer.get(0).html();
+        String updateTime = authorTimer.get(2).html();
+
+        titlePart.add(authorName);
+        titlePart.add(updateTime);
+
+        Element title2 = doc.getElementById("intro");
+        Elements introduce = title2.getElementsByTag("p");
+        String intro = introduce.html();
+
+        titlePart.add(intro);
+
+        return titlePart;
+
+
+    }
+
+
 }
