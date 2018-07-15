cd5f1ba548cb476a98be90bc2b566c5257a13a08
konglinghu777 <2710404562@qq.com>
2018-07-15 09:30:40 +0800

    J2ee learn .

---
+++ src/com/kong/useJsoup/model/parseBookDetial.java
@@ -0,0 +1,16 @@
+package com.kong.useJsoup.model;
+
+public class parseBookDetial {
+
+    private String bookId;
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public
+}
