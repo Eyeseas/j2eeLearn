293fc214ab459bd17de4017b127e7bc1d7fb7011
konglinghu777 <2710404562@qq.com>
2018-07-15 09:21:02 +0800

    J2ee learn .

--- web/js/common.js
+++ web/js/common.js
@@ -6,6 +6,7 @@
 
 $(document).ready(function () {
     $("#bookList .odd a").each(function () {
+        //截取pathname /xxxxx/
         this.href = "bookDetialServlet?bookid="+this.pathname.substring(1,this.pathname.length-1);
     })
 });
